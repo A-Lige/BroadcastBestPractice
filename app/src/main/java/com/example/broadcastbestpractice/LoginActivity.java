34eaeda5fe3c82edc4279f648f768be942a19d68
Yan <203676187@qq.com>
2020-08-09 20:24:13 +0800

    Just for test

--- app/src/main/java/com/example/broadcastbestpractice/LoginActivity.java
+++ app/src/main/java/com/example/broadcastbestpractice/LoginActivity.java
@@ -3,13 +3,23 @@ package com.example.broadcastbestpractice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.prefs.Preferences;
+
 public class LoginActivity extends BaseActivity {
+    private SharedPreferences preferences;
+    private SharedPreferences.Editor editor;
+    private CheckBox rememberPass;
+
     private EditText accountEdit;
     private EditText passwordEdit;
     private Button login;
@@ -22,12 +32,35 @@ public class LoginActivity extends BaseActivity {
         accountEdit=(EditText) findViewById(R.id.account);
         passwordEdit=(EditText) findViewById(R.id.password);
         login=(Button) findViewById(R.id.login);
+
+        preferences= PreferenceManager.getDefaultSharedPreferences(this);
+        rememberPass=(CheckBox)findViewById(R.id.remember_pass);
+        boolean isRemember=preferences.getBoolean("remember_password",false);
+        if (isRemember){
+            //将账号密码都设置到文本框中
+            String account=preferences.getString("account","");
+            String password=preferences.getString("password","");
+            accountEdit.setText(account);
+            passwordEdit.setText(password);
+            rememberPass.setChecked(true);
+        }
+
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String account=accountEdit.getText().toString();
                 String password=passwordEdit.getText().toString();
                 if (account.equals("abc")&&password.equals("123456")){
+                    //检查复选框是否选中
+                    editor=preferences.edit();
+                    if (rememberPass.isChecked()){
+                        editor.putBoolean("remember_password",true);
+                        editor.putString("account",account);
+                        editor.putString("password",password);
+                    }else {
+                        editor.clear();
+                    }
+                    editor.apply();
                     Intent intent=new Intent(LoginActivity.this,MainActivity.class);
                     startActivity(intent);
                     finish();
